18c203f6ed04362d413beca0d6253fafe1d3cd9d
Nick Toumpelis <nick@toumpelis.me.uk>
2011-12-24 17:09:02 +0200

    Added a target and scheme for logic unit tests.

---
+++ LogicTests/LogicTests.h
@@ -0,0 +1,13 @@
+//
+//  LogicTests.h
+//  LogicTests
+//
+//  Created by Nick Toumpelis on 24/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LogicTests : SenTestCase
+
+@end
---
+++ LogicTests/LogicTests.m
@@ -0,0 +1,32 @@
+//
+//  LogicTests.m
+//  LogicTests
+//
+//  Created by Nick Toumpelis on 24/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "LogicTests.h"
+
+@implementation LogicTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LogicTests");
+}
+
+@end
