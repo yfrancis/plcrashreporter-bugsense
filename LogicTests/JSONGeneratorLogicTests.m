940b602c156ad0a44ae9b49d8879fec166391c1f
Nick Toumpelis <nick@toumpelis.me.uk>
2011-12-27 12:47:39 +0200

    Minor test mods.

--- LogicTests/JSONGeneratorLogicTests.m
+++ LogicTests/JSONGeneratorLogicTests.m
@@ -1,32 +1,58 @@
-//
-//  LogicTests.m
-//  LogicTests
-//
-//  Created by Nick Toumpelis on 24/12/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
-//
+/*
+ 
+ JSONGeneratorLogicTests.m
+ BugSense-iOS
+ 
+ Copyright (c) 2011 BugSense Inc.
+ 
+ Permission is hereby granted, free of charge, to any person
+ obtaining a copy of this software and associated documentation
+ files (the "Software"), to deal in the Software without
+ restriction, including without limitation the rights to use,
+ copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following
+ conditions:
+ 
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
+ 
+ Author: Nick Toumpelis, nick@bugsense.com
+ 
+ */
 
-#import "LogicTests.h"
+#import "JSONGeneratorLogicTests.h"
 
-@implementation LogicTests
+@implementation JSONGeneratorLogicTests
 
-- (void)setUp
-{
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void) setUp {
     [super setUp];
     
     // Set-up code here.
 }
 
-- (void)tearDown
-{
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void) tearDown {
     // Tear-down code here.
     
     [super tearDown];
 }
 
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in LogicTests");
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void) testExample {
+    STFail(@"Unit tests are not implemented yet in JSONGeneratorLogicTests");
 }
 
 @end
